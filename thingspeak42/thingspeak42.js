8545e7140ed3af189c8822125a4b5f6293386e19
Exulansis <rusu.eugeniu@gmx.de>
2017-05-31 19:40:50 +0200

    Support for creating a new channel from within node red.

--- thingspeak42/thingspeak42.js
+++ thingspeak42/thingspeak42.js
@@ -2,9 +2,37 @@ var https = require('https');
 
 module.exports = function(RED) {
     function ThingSpeak42Node(config) {
-        RED.nodes.createNode(this,config);
+        RED.nodes.createNode(this, config);
         var node = this;
+        var creationPending = false;
 
+        node.option = config.dropdown;
+
+        // Options for new channel
+        node.name = config.name
+        node.description = config.description
+        node.public = config.public
+
+        node.newFieldNames = [
+          config.field1,
+          config.field2,
+          config.field3,
+          config.field4,
+          config.field5,
+          config.field6,
+          config.field7,
+          config.field8,
+        ]
+
+        node.meta = config.meta
+        node.tags = config.tags
+        node.url = config.url
+
+        node.lat = config.lat
+        node.long = config.long
+        node.elevation = config.elevation
+
+        // Options for appending to existing channel
         node.delay = config.delay;
         node.topics = [
             config.topic1,
@@ -16,6 +44,7 @@ module.exports = function(RED) {
             config.topic7,
             config.topic8
             ];
+
         node.endpoint = config.endpoint;
 
         node.timeout = null;
@@ -52,9 +81,107 @@ module.exports = function(RED) {
             }
         }
 
+        function createChannel() {
+          if (creationPending)
+            return Promise.reject("duplicate request") 
+
+          if (!node.credentials.accKey) {
+            node.status({fill:"red",shape:"dot",text:"No account API key"});
+            return Promise.reject("no key")
+          }
+
+          creationPending = true;
+          node.status({fill:"blue",shape:"dot",text:"Creating channel..."});
+
+          const options = {
+            port: 443,
+            hostname: "api.thingspeak.com",
+            path: "/channels.json",
+            method: "POST"
+          }
+
+          const req = https.request(options, (res) => {
+            if (res.statusCode !== 200)
+              return Promise.reject("status code", res.statusCode)
+
+            let body = ""
+            res.on("data", chunk => body += chunk)
+
+            res.on("end", () => {
+              try {
+                node.credentials.apiKey = JSON.parse(body).api_keys.find(key => key.write_flag).api_key
+              } catch(e) {
+                // TODO 
+                node.error(e)
+              }
+            })
+          });
+
+          req.on("error", e => 
+            console.warn("issues with creating channel")
+          )
+
+          let body = `api_key=${node.credentials.accKey}`
+          
+          // Adding the optional fields
+          body = appendIfFilled(body)
+
+          // Adding the individual fields
+          node.newFieldNames.forEach((name, index) => {
+            if(name) 
+              body += `&field${index + 1}=${name}`
+          })
+
+          return new Promise(resolve => {
+            return req.end(body, "utf8", () => {
+              node.option = "existing"
+              creationPending = false;
+              node.status({fill:"green",shape:"dot",text:"Channel created"});
+              return resolve()
+            })
+          })
+        }
+
+        function appendIfFilled(data) {
+          const map = [{
+            type: "public_flag", 
+            value: node.public
+          },{
+            type: "name",
+            value: node.name
+          }, {
+            type: "description",
+            value: node.description
+          }, {
+            type: "url",
+            value: node.url
+          }, {
+            type: "tags",
+            value: node.tags
+          }, {
+            type: "latitude",
+            value: node.lat
+          }, {
+            type: "longitude",
+            value: node.long
+          }, {
+            type: "elevation",
+            value: node.elevation
+          }, {
+            type: "metadata",
+            value: node.meta
+          }]
+
+          map.forEach(el => {
+            if (el.value) 
+              data += `&${el.type}=${el.value}`
+          })
+
+          return data
+        }
+
         function publishData() {
             node.status({fill:"blue",shape:"dot",text:"uploading data..."});
-
             var url = buildThingSpeakURL();
             stopTimer();
 
@@ -92,13 +219,32 @@ module.exports = function(RED) {
             }
         }
 
-        this.on('input', function(msg) {
+        function uploadData(msg) {
             for(i=0; i < node.topics.length; i++) {
                 if( msg.topic == node.topics[i] ) {
                     storeValue(i, msg.payload);
                     startTimer();
                 }
             }
+        }
+
+        this.on('input', function(msg) {
+          if (node.option === "new") {
+            return createChannel()
+              .then(() => uploadData(msg))
+              .catch(e => {
+                if (e !== "duplicate request")
+                  return node.status({fill:"red",shape:"dot",text:"Could not create channel"});
+
+                node.error(e)
+              }); 
+          }
+
+          if (!node.credentials.apiKey) {
+            return node.status({fill:"red",shape:"dot",text:"No channel api key"});
+          }
+
+          return uploadData(msg)
         });
 
         this.on('close', function() {
@@ -108,7 +254,8 @@ module.exports = function(RED) {
 
     RED.nodes.registerType("thingspeak42",ThingSpeak42Node, {
         credentials: {
-            apiKey: {type: "password"}
+            apiKey: {type: "password"},
+            accKey: {type: "password"}
         }
     });
 };
